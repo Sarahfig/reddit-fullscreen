3ea9c28d63d35dc06b4f14679e22a829c6e2def1
Spencer Watson <spencer@wattydev.com>
2015-09-30 09:09:10 -0700

    Add auth variables to session service

--- reddit/app/services/session.js
+++ reddit/app/services/session.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  auth: {
+    error: function(){
+      return localStorage.authError;
+    },
+    code: function() {
+      return localStorage.authCode;
+    },
+    state: function() {
+      return localStorage.authState;
+    },
+    token: function() {
+      return JSON.parse(localStorage.authToken);
+    }
+  }
 });
