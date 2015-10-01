70767b208072c108161b8163e91e96e7ccd88457
Spencer Watson <spencer@wattydev.com>
2015-10-01 08:44:33 -0700

    trying to get the post to work

--- reddit/app/services/api.js
+++ reddit/app/services/api.js
@@ -7,7 +7,8 @@ export default Ember.Service.extend({
       console.log(Ajax);
       return new Ajax().post('https://www.reddit.com/api/v1/access_token', 'grant_type=authorization_code&code=' + code + '&redirect_uri=http://localhost:4200/authorize', {
         headers: {
-          'Content-Type': 'multipart/form-data'
+          'Content-Type': 'multipart/form-data',
+          'Authorization': 'VVotW8Dp6WU6-w:ZFvRllL6c8qm0hcgfRxlIKSLkTo'
         }
       });
     },
