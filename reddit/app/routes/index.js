ca8b42db98709af880e0762fd3e1f03e9f0a98f5
Spencer Watson <spencer@wattydev.com>
2015-10-01 11:22:38 -0700

    Get front page data

--- reddit/app/routes/index.js
+++ reddit/app/routes/index.js
@@ -5,5 +5,38 @@ export default Ember.Route.extend({
     if(this.session.auth.needed()) {
       this.transitionTo('authenticate');
     }
+  },
+  model: function() {
+    return this.api.front.get().then(function(response) {
+      var model = {};
+      model.after = response.data.after;
+      model.list = response.data.children.map(function(item) {
+        var data = item.data,
+          parsed = {
+            author: data.author,
+            comments: data.permalink,
+            created: data.created,
+            domain: data.domain,
+            downs: data.downs,
+            id: data.id,
+            isSelf: data.is_self,
+            media: data.media,
+            embed: data.embed,
+            name: data.name,
+            numComments: data.numComments,
+            nsfw: data.over_18,
+            saved: data.saved,
+            score: data.score,
+            subreddit: data.subreddit,
+            subredditId: data.subreddit_id,
+            thumbnail: data.thumbnail,
+            title: data.title,
+            ups: data.ups,
+            url: data.url,
+          };
+        return parsed;
+      });
+      return model;
+    });
   }
 });
--- reddit/app/services/api.js
+++ reddit/app/services/api.js
@@ -1,19 +1,28 @@
 import Ember from 'ember';
 import Ajax from 'reddit/utils/ajax';
 
-var base = 'https://oauth.reddit.com/api/',
+var domain = 'https://oauth.reddit.com/',
+  base = domain + 'api/',
   token = function() {
     return 'Bearer ' + localStorage.authToken;
+  },
+  auth = function() {
+    return {
+      headers: {
+        'Authorization': token()
+      }
+    };
   };
 
 export default Ember.Service.extend({
   account: {
     getMe: function() {
-      return new Ajax().get(base + 'v1/me', null, {
-        headers: {
-          'Authorization': token()
-        }
-      });
+      return new Ajax().get(base + 'v1/me', null, auth());
+    }
+  },
+  front: {
+    get: function() {
+      return new Ajax().get(domain, null, auth());
     }
   }
 });
