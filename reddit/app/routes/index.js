8b6ce5418c4ce4752f7ced6d9759126125a0b5c8
Spencer Watson <spencer@wattydev.com>
2015-10-03 14:22:27 -0700

    Setting up infinite loading

--- reddit/app/routes/index.js
+++ reddit/app/routes/index.js
@@ -1,77 +1,91 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  beforeModel: function() {
-    if(this.session.auth.needed()) {
-      this.transitionTo('authenticate');
-    }
-  },
+	beforeModel: function() {
+		if(this.session.auth.needed()) {
+			this.transitionTo('authenticate');
+		}
+	},
 	model: function() {
+		var self = this;
 		return this.api.front.get().then(function(response) {
 			var model = {};
 			model.after = response.data.after;
-			model.list = response.data.children.map(function(item) {
-				var data = item.data,
-				parsed = {
-					author: data.author,
-					comments: data.permalink,
-					created: data.created,
-					domain: data.domain,
-					downs: data.downs,
-					id: data.id,
-					isSelf: data.is_self,
-					media: data.media,
-					embed: data.media_embed,
-					name: data.name,
-					numComments: data.num_comments,
-					nsfw: data.over_18,
-					saved: data.saved,
-					score: data.score,
-					subreddit: data.subreddit,
-					subredditId: data.subreddit_id,
-					thumbnail: data.thumbnail,
-					title: data.title,
-					ups: data.ups,
-					url: data.url,
-				};
-		  		if(parsed.thumbnail === 'self' || !parsed.thumbnail) {
-					parsed.hasThumbnail = false;
-				} else {
-					parsed.hasThumbnail = true;
-				}
-				if(!parsed.media) {
-					if(parsed.url.toLowerCase().match(/\.(jpg|png|gif)/g)) {
-						parsed.isImage = true;
-					} else if(parsed.url.indexOf('imgur.com/a/') !== -1){
-						parsed.isAlbum = true;
-					} else if(parsed.url.indexOf('imgur.com/') !== -1) {
-						parsed.isImage = true;
-						parsed.url = parsed.url + '.jpg';
-					} else if (parsed.url.indexOf('livememe.com/') !== -1) {
-						parsed.isImage = true;
-						var id = parsed.url.split('com/')[1];
-						parsed.url = 'http://e.lvme.me/' + id + '.jpg';
-					} else {
-						parsed.isArticle = true;
-						if(parsed.thumbnail === 'self' || !parsed.thumbnail) {
-							parsed.isArticleNoThumbnail = true;
-						} else {
-							parsed.isArticleThumbnail = true;
-						}
-					}
-				} else {
-					if(parsed.media.oembed.type === 'video') {
-						parsed.isVideo = true;
-						parsed.html = Ember.$('<div/>').html(parsed.media.oembed.html).text();
-					} else if(parsed.url.indexOf('imgur.com/a/') !== -1){
-						parsed.isAlbum = true;
-					} else {
-						console.log('unsupported media type', parsed);
-					}
-				}
-				return parsed;
-			});
+			model.list = self.parse(response.data.children);
 			return model;
 		});
+	},
+	parse: function(list) {
+		return list.map(function(item) {
+			var data = item.data,
+			parsed = {
+				author: data.author,
+				comments: data.permalink,
+				created: data.created,
+				domain: data.domain,
+				downs: data.downs,
+				id: data.id,
+				isSelf: data.is_self,
+				media: data.media,
+				embed: data.media_embed,
+				name: data.name,
+				numComments: data.num_comments,
+				nsfw: data.over_18,
+				saved: data.saved,
+				score: data.score,
+				subreddit: data.subreddit,
+				subredditId: data.subreddit_id,
+				thumbnail: data.thumbnail,
+				title: data.title,
+				ups: data.ups,
+				url: data.url,
+			};
+			if(parsed.thumbnail === 'self' || !parsed.thumbnail) {
+				parsed.hasThumbnail = false;
+			} else {
+				parsed.hasThumbnail = true;
+			}
+			if(!parsed.media) {
+				if(parsed.url.toLowerCase().match(/\.(jpg|png|gif)/g)) {
+					parsed.isImage = true;
+				} else if(parsed.url.indexOf('imgur.com/a/') !== -1){
+					parsed.isAlbum = true;
+				} else if(parsed.url.indexOf('imgur.com/') !== -1) {
+					parsed.isImage = true;
+					parsed.url = parsed.url + '.jpg';
+				} else if (parsed.url.indexOf('livememe.com/') !== -1) {
+					parsed.isImage = true;
+					var id = parsed.url.split('com/')[1];
+					parsed.url = 'http://e.lvme.me/' + id + '.jpg';
+				} else {
+					parsed.isArticle = true;
+					if(parsed.thumbnail === 'self' || !parsed.thumbnail) {
+						parsed.isArticleNoThumbnail = true;
+					} else {
+						parsed.isArticleThumbnail = true;
+					}
+				}
+			} else {
+				if(parsed.media.oembed.type === 'video') {
+					parsed.isVideo = true;
+					parsed.html = Ember.$('<div/>').html(parsed.media.oembed.html).text();
+				} else if(parsed.url.indexOf('imgur.com/a/') !== -1){
+					parsed.isAlbum = true;
+				} else {
+					console.log('unsupported media type', parsed);
+				}
+			}
+			return parsed;
+		});
+	},
+	actions: {
+		getMore: function() {
+			var after = this.get('context.after');
+			var self = this;
+			console.log(self);
+			this.api.front.getMore(after).then(function(response) {
+				console.log(response);
+			});
+		}
 	}
 });
--- reddit/dist/assets/reddit.js
+++ reddit/dist/assets/reddit.js
@@ -173,6 +173,11 @@ define('reddit/controllers/index', ['exports', 'ember'], function (exports, Embe
 			nextPost: function nextPost() {
 				if (this.currentIndex < this.get('model.list').length - 1) {
 					this.set('currentIndex', this.currentIndex + 1);
+
+					if (this.currentIndex === this.get('model.list').length - 2) {
+						//load more
+						this.send('getMore');
+					}
 				}
 			},
 			previousPost: function previousPost() {
@@ -400,72 +405,86 @@ define('reddit/routes/index', ['exports', 'ember'], function (exports, Ember) {
 			}
 		},
 		model: function model() {
+			var self = this;
 			return this.api.front.get().then(function (response) {
 				var model = {};
 				model.after = response.data.after;
-				model.list = response.data.children.map(function (item) {
-					var data = item.data,
-					    parsed = {
-						author: data.author,
-						comments: data.permalink,
-						created: data.created,
-						domain: data.domain,
-						downs: data.downs,
-						id: data.id,
-						isSelf: data.is_self,
-						media: data.media,
-						embed: data.media_embed,
-						name: data.name,
-						numComments: data.num_comments,
-						nsfw: data.over_18,
-						saved: data.saved,
-						score: data.score,
-						subreddit: data.subreddit,
-						subredditId: data.subreddit_id,
-						thumbnail: data.thumbnail,
-						title: data.title,
-						ups: data.ups,
-						url: data.url
-					};
-					if (parsed.thumbnail === 'self' || !parsed.thumbnail) {
-						parsed.hasThumbnail = false;
-					} else {
-						parsed.hasThumbnail = true;
-					}
-					if (!parsed.media) {
-						if (parsed.url.toLowerCase().match(/\.(jpg|png|gif)/g)) {
-							parsed.isImage = true;
-						} else if (parsed.url.indexOf('imgur.com/a/') !== -1) {
-							parsed.isAlbum = true;
-						} else if (parsed.url.indexOf('imgur.com/') !== -1) {
-							parsed.isImage = true;
-							parsed.url = parsed.url + '.jpg';
-						} else if (parsed.url.indexOf('livememe.com/') !== -1) {
-							parsed.isImage = true;
-							var id = parsed.url.split('com/')[1];
-							parsed.url = 'http://e.lvme.me/' + id + '.jpg';
-						} else {
-							parsed.isArticle = true;
-							if (parsed.thumbnail === 'self' || !parsed.thumbnail) {
-								parsed.isArticleNoThumbnail = true;
-							} else {
-								parsed.isArticleThumbnail = true;
-							}
-						}
-					} else {
-						if (parsed.media.oembed.type === 'video') {
-							parsed.isVideo = true;
-							parsed.html = Ember['default'].$('<div/>').html(parsed.media.oembed.html).text();
-						} else if (parsed.url.indexOf('imgur.com/a/') !== -1) {
-							parsed.isAlbum = true;
-						} else {
-							console.log('unsupported media type', parsed);
-						}
-					}
-					return parsed;
-				});
+				model.list = self.parse(response.data.children);
 				return model;
 			});
+		},
+		parse: function parse(list) {
+			return list.map(function (item) {
+				var data = item.data,
+				    parsed = {
+					author: data.author,
+					comments: data.permalink,
+					created: data.created,
+					domain: data.domain,
+					downs: data.downs,
+					id: data.id,
+					isSelf: data.is_self,
+					media: data.media,
+					embed: data.media_embed,
+					name: data.name,
+					numComments: data.num_comments,
+					nsfw: data.over_18,
+					saved: data.saved,
+					score: data.score,
+					subreddit: data.subreddit,
+					subredditId: data.subreddit_id,
+					thumbnail: data.thumbnail,
+					title: data.title,
+					ups: data.ups,
+					url: data.url
+				};
+				if (parsed.thumbnail === 'self' || !parsed.thumbnail) {
+					parsed.hasThumbnail = false;
+				} else {
+					parsed.hasThumbnail = true;
+				}
+				if (!parsed.media) {
+					if (parsed.url.toLowerCase().match(/\.(jpg|png|gif)/g)) {
+						parsed.isImage = true;
+					} else if (parsed.url.indexOf('imgur.com/a/') !== -1) {
+						parsed.isAlbum = true;
+					} else if (parsed.url.indexOf('imgur.com/') !== -1) {
+						parsed.isImage = true;
+						parsed.url = parsed.url + '.jpg';
+					} else if (parsed.url.indexOf('livememe.com/') !== -1) {
+						parsed.isImage = true;
+						var id = parsed.url.split('com/')[1];
+						parsed.url = 'http://e.lvme.me/' + id + '.jpg';
+					} else {
+						parsed.isArticle = true;
+						if (parsed.thumbnail === 'self' || !parsed.thumbnail) {
+							parsed.isArticleNoThumbnail = true;
+						} else {
+							parsed.isArticleThumbnail = true;
+						}
+					}
+				} else {
+					if (parsed.media.oembed.type === 'video') {
+						parsed.isVideo = true;
+						parsed.html = Ember['default'].$('<div/>').html(parsed.media.oembed.html).text();
+					} else if (parsed.url.indexOf('imgur.com/a/') !== -1) {
+						parsed.isAlbum = true;
+					} else {
+						console.log('unsupported media type', parsed);
+					}
+				}
+				return parsed;
+			});
+		},
+		actions: {
+			getMore: function getMore() {
+				var after = this.get('context.after');
+				var self = this;
+				console.log(self);
+				this.api.front.getMore(after).then(function (response) {
+					console.log(response);
+				});
+			}
 		}
 	});
 
@@ -496,6 +515,9 @@ define('reddit/services/api', ['exports', 'ember', 'reddit/utils/ajax'], functio
 		front: {
 			get: function get() {
 				return new Ajax['default']().get(domain, null, auth());
+			},
+			getMore: function getMore(after) {
+				return new Ajax['default']().get(domain, { after: after }, auth());
 			}
 		},
 		post: {
@@ -2271,7 +2293,7 @@ catch(err) {
 if (runningTests) {
   require("reddit/tests/test-helper");
 } else {
-  require("reddit/app")["default"].create({"name":"reddit","version":"0.0.0+8a9ecac5"});
+  require("reddit/app")["default"].create({"name":"reddit","version":"0.0.0+ac586fdc"});
 }
 
 /* jshint ignore:end */
